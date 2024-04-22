fdb82a9e42abcb0b51c8f9336b4b32e4c7196b87
song_hsu <song_hsu@163.com>
2024-04-22 16:03:25 +0800

    周一 2024/04/22 16:03:25.55

--- replace_catnls.bat
+++ replace_catnls.bat
@@ -1,5 +1,6 @@
 cd /d %~dp0
 
 xcopy /s /e /c /y /h /r /d Fixed_Language_Package_B426_FD03\*.* "C:\Program Files\Dassault Systemes\B426\win_b64\resources\msgcatalog\Simplified_Chinese\"
+xcopy /s /e /c /y /h /r /d Fixed_Language_Package_B426_FD03\*.* "C:\Program Files\Dassault Systemes\B426_Cloud\win_b64\resources\msgcatalog\Simplified_Chinese\"
 
 pause
